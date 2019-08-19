16c2ab15c36e2bb79c403c4775bd6ae284998165
gustavo <gustavo@GUSTAVO-PC>
2019-08-19 11:12:51 -0300

    .

--- LojaGratis/LojaGratis/Models/Item.cs
+++ LojaGratis/LojaGratis/Models/Item.cs
@@ -6,6 +6,8 @@ namespace LojaGratis.Models
     {
         public string Id { get; set; }
         public string Text { get; set; }
-        public string Description { get; set; }  
+        public string Description { get; set; }
+
+        public string Teste { get; set; }
     }
 }
